682811695108395ec1975b5575c0a44a0ddd91a2
Mishywayu <michellewayua2@gmail.com>
2025-05-30 21:40:05 +0300

    Python Bootcamp notes

--- comparison_operators.py
+++ comparison_operators.py
@@ -12,3 +12,19 @@ elif temperature < 10:
     print("It's a cold day")
 else:
     print("It's neither hot nor cold")
+
+
+# Exercise
+"""
+if name is less han 3 characters long, name must be atleast 3 characters
+otherwise if it's more than 50 characters long, name can be a maximum of 50 characters
+otherwise, name looks good!
+"""
+name = input("Input your name: ")
+
+if len(name) < 3:
+    print("Name must be atleast 3 characters.")
+elif len(name) > 50:
+    print("Name can be a maximum of 50 characters")
+else:
+    print("Name looks good!")
