b8773afa5891521b44556729e2a7fd6d6c364a49
Mishywayu <michellewayua2@gmail.com>
2025-06-02 17:29:33 +0300

    First Project

--- weight_convertor.py
+++ weight_convertor.py
@@ -17,3 +17,11 @@ else:
 
 
 # Mosh's Solution
+weight = int(input('Weight: '))
+unit = input('(L)bs or (K)g: ')
+if unit.upper() == 'L':
+    converted = weight * 0.45
+    print(f"You are {converted} kilos")
+else:
+    converted = weight / 0.45
+    print(f"You are {converted} pounds")
